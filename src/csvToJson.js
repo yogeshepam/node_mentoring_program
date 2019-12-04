2fee1d9fb10efc4723d8c37ed9216aa6d692d1cb
Yogesh Devgun <YOGESH.DEVGUN@KNAB.NL>
2019-12-04 15:12:23 +0200

    Added task 1.2 covering: read csv file, convert csv file to json object line to line, write file new txt file, read/write errors.

---
+++ src/csvToJson.js
@@ -0,0 +1,35 @@
+const csvtojson = require("csvtojson");
+const fs = require("fs");
+
+const csvToJson = () => {
+  if (fs.existsSync("./example.txt")) {
+    fs.unlinkSync("./example.txt");
+  }
+  csvtojson()
+    .fromFile("./node_mentoring_t1_2_input_example.csv")
+    .subscribe(
+      json => {
+        appendFile(json);
+      },
+      err => {
+        console.log(err);
+        throw new Error(err);
+      },
+      () => {
+        console.log(
+          `Successfully created file named example.txt with data from csv to json`
+        );
+      }
+    );
+};
+
+const appendFile = json => {
+  fs.appendFile("example.txt", JSON.stringify(json) + "\n", err => {
+    if (err) {
+      console.log("Failed to append txt file, ", err);
+      throw new Error(err);
+    }
+  });
+};
+
+console.log(csvToJson());
