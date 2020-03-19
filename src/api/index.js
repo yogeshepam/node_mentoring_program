aaa5fd4e18fa4d7e0dfc3840a633864013849f15
yogeshepam <53252472+yogeshepam@users.noreply.github.com>
2020-03-19 22:34:28 +0200

    Removed repetitive import

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import auth from './routes/auth';
 import group from './routes/group';
 import user from './routes/user';
-import group from './routes/group';
 
 export default () => {
     const app = Router();
